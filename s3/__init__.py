6b97d89153dce91be8566bc6a872091c29ce96b1
Jared Contrascere <jcontra@gmail.com>
2015-03-03 14:02:34 -0500

    Clean up logger statements.

--- s3/__init__.py
+++ s3/__init__.py
@@ -9,9 +9,9 @@ DJANGO_ENGINE = core.settings.DATABASES['default']['ENGINE']
 logger = logging.getLogger(__name__)
 
 def connect_s3():
-    logger.info('Connecting to the Amazon Simple Storage Service (Amazon S3) service.')
+    logger.info('Connecting to the Amazon Simple Storage Service (Amazon S3).')
     s3 = boto.connect_s3(aws_access_key_id=core.args.key_id,
                          aws_secret_access_key=core.args.key)
-    logger.info('Connected to the Amazon S3 service.')
+    logger.info('Connected to Amazon S3.')
 
     return s3
