309cc91158e100779333beca4b7871ef3217658a
Jared Contrascere <jcontra@gmail.com>
2015-03-03 13:39:47 -0500

    Refactor database creation.

--- canvas.py
+++ canvas.py
@@ -5,9 +5,9 @@
 import sys
 import logging
 import boto
-import core
 import vpc
 import ec2
+import rds
 
 __author__ = 'Jared Contrascere'
 __copyright__ = 'Copyright 2015, LibreTees, LLC'
@@ -16,75 +16,25 @@ __license__ = 'GPLv3'
 logger = logging.getLogger(__name__)
 
 def main():
-    PROJECT_NAME = core.PROJECT_NAME
-    django_engine = core.settings.DATABASES['default']['ENGINE']
-
-    if django_engine not in ['django.db.backends.postgresql_psycopg2' \
-                            ,'django.db.backends.mysql' \
-                            ,'django.db.backends.sqlite3' \
-                            ,'django.db.backends.oracle']:
-        logger.error('Unknown database engine (%s).' % django_engine, exc_info=True)
-        sys.exit(1)
-    else:
-        logger.info('Provisioning RDS instance for Django engine (%s).' % django_engine)
 
     vpc_connection = vpc.connect_vpc()
     ec2_connection = ec2.connect_ec2()
+    rds_connection = rds.connect_rds()
 
     vpcs = vpc_connection.get_all_vpcs()
     default_vpc = vpcs[0]
-
     default_subnets = vpc_connection.get_all_subnets(filters={
                                                          'vpcId': default_vpc.id
                                                      })
-    # for subnet in default_subnets:
-    #     print(type(subnet), subnet, subnet.id)
 
+    pg = rds.create_db_parameter_group(rds_connection)
+    subnet = rds.create_db_subnet_group(rds_connection, default_subnets)
 
+    db = rds.create_db_instance(rds_connection,
+                                pg,
+                                subnet)
 
-    logger.info('Connecting to the Amazon Relational Database Service (Amazon RDS) service.')
-    rds = boto.connect_rds2(aws_access_key_id=core.args.key_id,
-                            aws_secret_access_key=core.args.key)
-    logger.info('Connected to the Amazon RDS service.')
-
-    # Not required for Amazon VPC
-    # db_security_group_name = '-'.join(['gp', PROJECT_NAME.lower(), core.args.environment.lower(), 'db'])
-    # db_security_group_description = ' '.join([PROJECT_NAME, 'DB Security Group'])
-    # sg = rds.create_db_security_group(db_security_group_name,        #db_security_group_name
-    #                                   db_security_group_description) #db_security_group_description
-
-    # Affected by boto Issue #2677 : https://github.com/boto/boto/issues/2677
-    aws_engine = {
-        'django.db.backends.postgresql_psycopg2': 'postgres9.3',
-        'django.db.backends.mysql':               'MySQL5.6',
-        'django.db.backends.oracle':              'oracle-se1-11.2',
-    }
-    db_parameter_group_name = '-'.join(['pg', PROJECT_NAME.lower(), core.args.environment.lower(), 'db'])
-    pg = rds.create_db_parameter_group(db_parameter_group_name,                                 #db_parameter_group_name
-                                       aws_engine[django_engine],                               #db_parameter_group_family
-                                       description=' '.join([PROJECT_NAME, 'Parameter Group'])) #description
-
-    db_subnet_group_name = '-'.join(['net', PROJECT_NAME.lower(), core.args.environment.lower(), 'db'])
-    rds.create_db_subnet_group(db_subnet_group_name,                       #db_subnet_group_name
-                              ' '.join([PROJECT_NAME, 'DB Subnet Group']), #db_subnet_group_description
-                              [subnet.id for subnet in default_subnets])   #subnet_ids
-
-    aws_engine = {
-        'django.db.backends.postgresql_psycopg2': 'postgres',
-        'django.db.backends.mysql':               'MySQL',
-        'django.db.backends.oracle':              'oracle-se1',
-    }
-    # inst = rds.create_db_instance('dbinst1',                 #db_instance_identifier
-    #                               5,                         #allocated_storage
-    #                               'db.t2.micro',             #db_instance_class
-    #                               aws_engine[django_engine], #engine
-    #                               'username',                #master_username
-    #                               'password',                #master_user_password
-    #                               #db_security_groups=[db_security_group_name],
-    #                               db_subnet_group_name=db_subnet_group_name,
-    #                               db_parameter_group_name=db_parameter_group_name)
-
-    rs = rds.describe_db_instances()
+    rs = rds_connection.describe_db_instances()
     print(type(rs), rs)
 
 if __name__ == '__main__':
---
+++ rds/__init__.py
@@ -0,0 +1,75 @@
+import sys
+import logging
+import boto
+import core
+
+PROJECT_NAME = core.PROJECT_NAME
+DJANGO_ENGINE = core.settings.DATABASES['default']['ENGINE']
+
+logger = logging.getLogger(__name__)
+
+def connect_rds():
+    logger.info('Connecting to the Amazon Relational Database Service (Amazon RDS) service.')
+    rds = boto.connect_rds2(aws_access_key_id=core.args.key_id,
+                            aws_secret_access_key=core.args.key)
+    logger.info('Connected to the Amazon RDS service.')
+    
+    return rds
+
+def provision_db():
+    if DJANGO_ENGINE not in ['django.db.backends.postgresql_psycopg2' \
+                            ,'django.db.backends.mysql' \
+                            ,'django.db.backends.sqlite3' \
+                            ,'django.db.backends.oracle']:
+        logger.error('Unknown database engine (%s).' % DJANGO_ENGINE, exc_info=True)
+        sys.exit(1)
+    else:
+        logger.info('Provisioning RDS instance for Django engine (%s).' % DJANGO_ENGINE)
+        
+def create_db_parameter_group(rds):
+    # Affected by boto Issue #2677 : https://github.com/boto/boto/issues/2677
+    aws_engine = {
+        'django.db.backends.postgresql_psycopg2': 'postgres9.3',
+        'django.db.backends.mysql':               'MySQL5.6',
+        'django.db.backends.oracle':              'oracle-se1-11.2',
+    }
+    db_parameter_group_name = '-'.join(['pg', PROJECT_NAME.lower(), core.args.environment.lower(), 'db'])
+    pg = rds.create_db_parameter_group(db_parameter_group_name,                                 #db_parameter_group_name
+                                       aws_engine[DJANGO_ENGINE],                               #db_parameter_group_family
+                                       description=' '.join([PROJECT_NAME, 'Parameter Group'])) #description
+    return pg
+
+def create_db_subnet_group(rds, default_subnets):
+    db_subnet_group_name = '-'.join(['net', PROJECT_NAME.lower(), core.args.environment.lower(), 'db'])
+    subnet = rds.create_db_subnet_group(db_subnet_group_name,                        #db_subnet_group_name
+                                        ' '.join([PROJECT_NAME, 'DB Subnet Group']), #db_subnet_group_description
+                                        [subnet.id for subnet in default_subnets])   #subnet_ids
+    return subnet
+
+def create_db_instance(rds, db_parameter_group, db_subnet_group):
+    aws_engine = {
+        'django.db.backends.postgresql_psycopg2': 'postgres',
+        'django.db.backends.mysql':               'MySQL',
+        'django.db.backends.oracle':              'oracle-se1',
+    }
+
+    db_parameter_group_name = db_parameter_group['CreateDBParameterGroupResponse']\
+                                                ['CreateDBParameterGroupResult']\
+                                                ['DBParameterGroup']\
+                                                ['DBParameterGroupName']
+
+    db_subnet_group_name = db_subnet_group['CreateDBSubnetGroupResponse']\
+                                          ['CreateDBSubnetGroupResult']\
+                                          ['DBSubnetGroup']\
+                                          ['DBSubnetGroupName']
+
+    instance = rds.create_db_instance('dbinst1',                 #db_instance_identifier
+                                      5,                         #allocated_storage
+                                      'db.t2.micro',             #db_instance_class
+                                      aws_engine[DJANGO_ENGINE], #engine
+                                      'username',                #master_username
+                                      'password',                #master_user_password
+                                      #db_security_groups=[db_security_group_name],
+                                      db_subnet_group_name=db_subnet_group_name,
+                                      db_parameter_group_name=db_parameter_group_name)
+    return instance
