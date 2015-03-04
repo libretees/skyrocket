29b1ed89dc7e2f6975efffdce114b7d3f22a728a
Jared Contrascere <jcontra@gmail.com>
2015-03-04 13:45:57 -0500

    Create basic EC2 instance.

--- canvas.py
+++ canvas.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """canvas.py: Provision AWS environments for Django projects."""
 
+import random
 import logging
 import core
 import vpc
@@ -22,13 +23,21 @@ def main():
     rds_connection = rds.connect_rds()
     s3_connection = s3.connect_s3()
 
-    # s3.make_tarfile('.'.join([s3.PROJECT_NAME, 'tar', 'gz']), s3.PROJECT_DIRECTORY)
+    vpcs = vpc_connection.get_all_vpcs()
+    default_vpc = vpcs[0]
+    default_subnets = vpc_connection.get_all_subnets(filters={
+                                                         'vpcId': default_vpc.id
+                                                     })
 
-    # vpcs = vpc_connection.get_all_vpcs()
-    # default_vpc = vpcs[0]
-    # default_subnets = vpc_connection.get_all_subnets(filters={
-    #                                                      'vpcId': default_vpc.id
-    #                                                  })
+    ec2_instance_name = '-'.join(['ec2', core.PROJECT_NAME.lower(), core.args.environment.lower(), '%x' % random.randrange(2**32)])
+    reservation = ec2_connection.run_instances('ami-9a562df2',
+                                               instance_type='t2.micro',
+                                               subnet_id=default_subnets[0].id)
+
+    instance = reservation.instances[-1]
+    ec2_connection.create_tags([instance.id], {"Name": ec2_instance_name})
+
+    # s3.make_tarfile('.'.join([s3.PROJECT_NAME, 'tar', 'gz']), s3.PROJECT_DIRECTORY)
 
     # pg = rds.create_db_parameter_group(rds_connection)
     # subnet = rds.create_db_subnet_group(rds_connection, default_subnets)
