21b39abee7f720f048d639b6bf41eea0e6603b3f
Jared Contrascere <jcontra@gmail.com>
2015-04-19 22:19:41 -0400

    Allow 'ephemeral' decorator to be invoked.

--- sky/decorators.py
+++ sky/decorators.py
@@ -5,12 +5,31 @@ import core
 
 logger = logging.getLogger(__name__)
 
-def ephemeral(func):
-    @functools.wraps(func)
-    def decorator(*args, **kwargs):
-        core.CREATION_MODE = core.EPHEMERAL
-        return func(*args, **kwargs)
-    logging.info('Decorated (%s) as an \'Ephemeral\' Creation Mode function.' % func.__name__)
+EPHEMERAL = core.EPHEMERAL
+PERMANENT = core.PERMANENT
+
+def ephemeral(*args, **kwargs):
+    # Determine whether or not the decorator was invoked.
+    invoked = bool(args and not callable(args[0]) or kwargs)
+
+    if not invoked:
+        # Wrap the function in a decorator that sets the infrastructure creation mode to EPHEMERAL.
+
+        function, args = args[0], ()
+        @functools.wraps(function)
+        def decorator(*args, **kwargs):
+            core.CREATION_MODE = EPHEMERAL
+            return function(*args, **kwargs)
+        logging.info('Decorated (%s) as a \'Ephemeral\' Creation Mode function.' % function.__name__)
+    else:
+        # Define a decorator that will wrap a function in an Ephemeral Infrastructure object.
+
+        def decorator(function):
+            infrastructure = Infrastructure(function, *args, **kwargs)
+            infrastructure.category = EPHEMERAL
+            logging.info('Decorated (%s) as an Ephemeral Infrastructure object.' % function.__name__)
+            return infrastructure
+
     return decorator
 
 def permanent(func):
