0d1e6978b07285e58c893909343c5175610d5a88
Jared Contrascere <jcontra@gmail.com>
2015-04-25 20:32:14 -0400

    Add exception handling to sky.ReadyObject.

--- sky/state.py
+++ sky/state.py
@@ -3,25 +3,30 @@ import sys
 import logging
 from .utils import parse_arguments
 
-class Ready(dict):
+logger = logging.getLogger(__name__)
+
+class ReadyObject(dict):
 
     def __init__(self):
-        super(Ready, self).__init__()
+        super(ReadyObject, self).__init__()
 
     def __getattr__(self, attr):
-        return self[attr]
+        try:
+            value = self[attr]
+        except KeyError as error:
+            logger.error('%s is not available.')
+        return value
 
-ready = Ready()
+ready = ReadyObject()
 
 CREATION_MODE = None
 EPHEMERAL = 'ephemeral'
 PERMANENT = 'permanent'
 
-logger = logging.getLogger(__name__)
-
 args = parse_arguments()
 PROJECT_NAME = os.path.abspath(os.path.expanduser(args.directory)).split(os.sep)[-1].lower()
 PROJECT_DIRECTORY = os.path.abspath(os.path.expanduser(args.directory)).lower()
 ENVIRONMENT = args.environment.lower()
+AWS_ACCOUNT_ID = args.account_id
 AWS_ACCESS_KEY_ID = args.key_id
 AWS_SECRET_ACCESS_KEY = args.key
