b32f81094494b080afca4a34ac28c2d5d24ab5b2
Jared Contrascere <jcontra@gmail.com>
2015-04-26 18:04:03 -0400

    Package for release.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'sky',
+    packages = ['sky'],
+    version = '0.1',
+    description = 'Sky is a tool that allows quick and repeatable deployment of cloud resources.',
+    author = 'Jared Contrascere',
+    author_email = 'jcontra@gmail.com',
+    url = 'https://github.com/libretees/sky',
+
+    install_requires=['boto==2.36.0'],
+    entry_points={
+        'console_scripts': [
+            'sky = sky.main:main',
+        ]
+    },
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Clustering',
+          'Topic :: System :: Software Distribution',
+          'Topic :: System :: Systems Administration',
+    ],
+)
