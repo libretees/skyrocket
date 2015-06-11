df481e576047c8eccb6471271541f1d93e8be963
Jared Contrascere <jcontra@gmail.com>
2015-06-10 21:59:27 -0400

    Enable Sphinx documentation coverage extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
